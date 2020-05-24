ddf3f59079ecc0010b51ccdd17512fb536c11db0
alberto <alberto.valenti@varprime.com>
2020-05-24 23:53:33 +0200

    GetGetSharedKeyLiteHead

--- GetGetSharedKeyLiteHead.cs
+++ GetGetSharedKeyLiteHead.cs
@@ -39,9 +39,18 @@ namespace com.businesscentral
 
             #region GetSharedKeyLite
             var sb = new StringBuilder();
-            var urlForMacEvaluationGet = String.Format("/{0}/{1}/{2}/sample3.txt", config.accountName, config.shareName, config.workingFolder);
+            var urlForMacEvaluationGetFileExist = String.Format("/{0}/{1}/{2}/sample3.txt", config.accountName, config.shareName, config.workingFolder);
+            var urlForMacEvaluationGetDontExist = String.Format("/{0}/{1}/{2}/dontexist.txt", config.accountName, config.shareName, config.workingFolder);
+            var urlForMacEvaluationGetFolderExist = String.Format("/{0}/{1}/{2}", config.accountName, config.shareName, config.workingFolder);
+            var urlForMacEvaluationGetFolderDoesNotExist = String.Format("/{0}/{1}/{2}", config.accountName, config.shareName, "nofoo");
             var contentUrl = string.Empty;
-            sb.AppendLine(_azurestorage.GetSharedKeyLiteHead(config, urlForMacEvaluationGet, contentUrl));
+            sb.AppendLine(_azurestorage.GetSharedKeyLiteHead(config, urlForMacEvaluationGetFileExist, contentUrl));
+            sb.AppendLine("-----------------------------------------------------------------------------");
+            sb.AppendLine(_azurestorage.GetSharedKeyLiteHead(config, urlForMacEvaluationGetDontExist, contentUrl));
+            sb.AppendLine("-----------------------------------------------------------------------------");
+            sb.AppendLine(_azurestorage.GetSharedKeyLiteHead(config, urlForMacEvaluationGetFolderExist, contentUrl));
+            sb.AppendLine("-----------------------------------------------------------------------------");
+            sb.AppendLine(_azurestorage.GetSharedKeyLiteHead(config, urlForMacEvaluationGetFolderDoesNotExist, contentUrl));
 
             return new OkObjectResult(sb.ToString());
             #endregion
